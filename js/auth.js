eeeca028de2e54fab9eda65eefc9a31cb61c0b48
ROGERIO JOSE <57935315+Rogerio-17@users.noreply.github.com>
2023-08-29 08:18:21 -0300

    Adicionando os arquivos do projeto (#1)
    * Initial commit
    
    * "Acionando telas de login e cadastro"
    
    * Finalizado validações de inputs
    
    * finalizado validações de inputs
    
    * Revisão de CSS e Adição da Index
    
    * Revisão de CSS e Adição da Index
    
    * Correções de CSS
    
    * Correções de CSS
    
    * Tela de dados inicio layout
    
    * Adicionando validação de login e tela de loading
    
    * Finalizado autenticação e criação de usuario no BD
    
    * Criação do grafico
    
    * Adicionando media ao grafico
    
    * Corrigindo erro de criação de user
    
    * Adicionando recuperação de senha
    
    * Finalizando layout da tela de cadastro
    
    * Implemntando logiga no botão de cadastrar
    
    * Finalizando PopUp de cadastro de despesa/entrada
    
    * Adicionando arquivos
    
    * movendo license e readme
    
    * Ajustando links
    
    * Adicionando algumas edições feitas por antony
    
    * estilizando o botão de transações
    
    ---------
    
    Co-authored-by: Kennedy <67836881+VictorKennedy-Git@users.noreply.github.com>
    Co-authored-by: kennedy <victorsilvasm92@gmail.com>

---
+++ js/auth.js
@@ -0,0 +1,51 @@
+function auth(email, senha) {
+  // Exibe atela de carregamento
+  showLoading();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email.value, senha.value)
+    .then((response) => {
+      //Fecha tela de carregamento
+      hideLoading();
+      //Direciona para Pagina principal
+      window.location.href = "/main.html";
+    })
+    .catch((error) => {
+      //Fecha tela de carregamento
+      hideLoading();
+
+      //Usuario e senha incorretos
+      if (error.code == "auth/user-not-found") {
+        erroDeLogin.innerHTML = "Usúario não foi encontrado";
+        senha.style.color = "red";
+        senha.style.border = "2px solid red";
+        recuperaSenha.innerHTML = ""
+        email.style.color = "red";
+        email.style.border = "2px solid red";
+
+        // Senha incorreta
+      } else if (error.code == "auth/wrong-password") {
+        erroDeLogin.innerHTML = "Senha incorreta";
+        recuperaSenha.innerHTML = "Recuperar senha"
+        senha.style.color = "red";
+        senha.style.border = "2px solid red";
+
+        // Erro de rede
+      } else if (error.code == "auth/network-request-failed") {
+        erroDeLogin.innerHTML = "Erro na conexão! Verifique sua rede.";
+        recuperaSenha.innerHTML = ""
+
+        // Usuario tentou logar varias vezes com senha incorreta
+      } else if (error.code == "auth/too-many-requests") {
+        erroDeLogin.innerHTML = "Conta temporariamente bloqueada, devido a quantidade de tentativas de login incorreta. Para acessar agora redefina sua senha!";
+        recuperaSenha.innerHTML = ""
+        recuperaSenha.innerHTML = "Recuperar senha"
+
+        // Caso dê erro desconhecido
+      }
+       else {
+        erroDeLogin.innerHTML = "Erro desconheido " + error.message;
+        recuperaSenha.innerHTML = ""
+      }
+    });
+}
---
+++ js/login-cadastro.js
@@ -0,0 +1,135 @@
+let email = document.querySelector("#email");
+let senha = document.querySelector("#senha");
+let erroDeLogin = document.querySelector("#erro");
+let recuperaSenha = document.querySelector("#recuperar_senha");
+let botao = document.querySelector("#btn");
+let popup = document.querySelector("#popup");
+let imgPop = document.querySelector("#img")
+
+// Quando o campo não existir vai passar o valor de false para a variavel
+let dataDeNascimento = document.querySelector("#date") || false;
+let txtDoPopUp = document.querySelector("#txtDoPopUp");
+
+let btnFecharPopup = document.querySelector("#btnFecharPopup");
+let problemaEmail = "";
+let problemasSenha = "";
+
+// Verifica se os campos de inputs estão vazios
+function verificaCamposInput() {
+  //Valida email em tempo real
+  email.addEventListener("input", () => {
+    const emails = email.value;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (emailRegex.test(emails)) {
+      email.style.color = "#049dd9";
+      email.style.border = "2px solid #049dd9";
+      erroDeLogin.innerHTML = "";
+      problemaEmail = "";
+    } else {
+      email.style.color = "red";
+      email.style.border = "2px solid red";
+
+      // //Caso o email esteja incorreto, adionar o valor verdadeiro para verificar quando o cliente clicar em logar/cadastrar
+      problemaEmail = true;
+    }
+  });
+
+  //Valida se a senha tem quantidade minima de caracteres permitidas em tempo real
+  senha.addEventListener("input", () => {
+    if (senha.value.length < 6) {
+      senha.style.color = "red";
+      senha.style.border = "2px solid red";
+      erroDeLogin.innerHTML = "";
+
+      //Caso senha esteja incorreta, adionar o valor verdadeiro para verificar quando o cliente clicar em logar/cadastrar
+      problemasSenha = true;
+    } else {
+      senha.style.color = "#049dd9";
+      senha.style.border = "2px solid #049dd9";
+      problemasSenha = "";
+    }
+  });
+
+  // Quando é clicado no botão de login/cadastro verifica os campos
+  botao.addEventListener("click", () => {
+    //Para não da o refresh na aba
+    event.preventDefault();
+
+    //Verifica se possui inputs vazios
+    if (email.value != "" && senha.value != "") {
+      //Verifica se o email informado foi validado
+      if (problemaEmail == true) {
+        popup.style.display = "flex";
+        txtDoPopUp.textContent = "O email informado não é válido";
+
+        //Verifica se a senha informada foi validada
+      } else if (problemasSenha == true) {
+        popup.style.display = "flex";
+        txtDoPopUp.textContent = "A senha informada é menor que o permitido!";
+
+        // Verifica se a data de nascimento foi informada
+      } else if (dataDeNascimento.value == "") {
+        popup.style.display = "flex";
+        txtDoPopUp.textContent = "Por favor! Informe a sua data de nascimento";
+
+        //se estiver tudo OK vai seguir com a logica do banco que vai ser adicionada abaixo
+      } else {
+        if (botao.value == "logar") {
+          //Auntentica o usuario
+          auth(email, senha);
+        } else {
+          //Cadastrar
+          register(email, senha);
+        }
+      }
+    } else {
+      //Se possuir campos vazios passa a mensagem para preencher todos os campos
+      popup.style.display = "flex";
+      txtDoPopUp.textContent = "Preencha todos os campos!";
+    }
+  });
+}
+
+// Recuperar senha do usuario
+function recuperarSenha() {
+  recuperaSenha.addEventListener("click", () =>{
+    showLoading();
+    firebase
+      .auth()
+      .sendPasswordResetEmail(email.value)
+      .then(() => {
+        hideLoading();
+        popup.style.display = "flex";
+        imgPop.src = "assets/check.png"
+        txtDoPopUp.textContent = "Email para recuperação de senha enviado com sucesso!";
+      }).catch((err) => {
+        hideLoading();
+        alert("erroo")
+      })
+  })
+
+}
+
+recuperarSenha()
+// Fecha o popup
+function fecharPoup() {
+  btnFecharPopup.addEventListener("click", () => {
+    event.preventDefault();
+    const popup = document.getElementById("popup");
+    popup.style.display = "none";
+  });
+}
+
+//Matem o usuario logado, quando ele tentar acessar login sem deslogar antes
+function manterUsuarioLogado() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      window.location.href = "/main.html";
+    }
+  });
+}
+manterUsuarioLogado();
+
+fecharPoup();
+verificaCamposInput();
