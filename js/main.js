2ffc309e0481c72296c5cacb98fb304e751ee53a
kennedy <victorsilvasm92@gmail.com>
2023-08-31 14:13:04 -0300

    Resolução de Bugs de Conflito

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let logout = document.querySelector("#logout");
+
+
+// SELÇOÕES DE ELEMENTOS DE FORMULARIO
 let bntDeCadastroDeValor = document.querySelector("#cadastroValor");
 let popupDeCadastroDeValor = document.querySelector("#popupDeCadastroValor");
 let cadastrarValor = document.querySelector("#cadastraValor");
@@ -134,15 +137,6 @@ function cadastroDeEntradaOuSaida(){
 cadastroDeEntradaOuSaida()
 
 
-
-
-
-
-
-
-
-
-
 function cadastroDeEntradaOuSaida() {
   //Abre o Popup para cadastro de valor
   bntDeCadastroDeValor.addEventListener("click", () => {
@@ -176,17 +170,26 @@ function cadastraTransacao() {
   let botaoGerarCadastro = document.querySelector("#cadastraValor");
   let count = 0;
 
+  function erroCadastro(){
+    window.alert("Verifique as informações Cadastradas")
+  }
   botaoGerarCadastro.addEventListener("click", () => {
     // Pega os campos que o popup possui
     let titulo = document.querySelector("#titleCadatro");
     let campoSelecionavel = document.querySelector("#selectCadastro");
     let valor = document.querySelector("#valueCadastro");
     let categoria = document.querySelector("#categoryCadastro");
+    
+    
+    const paragraphs = document.querySelectorAll('.form_DE p');
 
-    //Sequencia de validações para saber se exite algum campo vazio dentro do Popup
+    //Sequencia de validações para saber se exite algum campo vazio dentro do Popup    
     if (titulo.value == "") {
       titulo.style.border = "2px solid red";
       popupDeCadastroDeValor.style.display = "flex";
+      paragraphs[0].style.color = 'red'
+      return erroCadastro()
+      
     } else {
       titulo.style.border = "transparent";
     }
@@ -194,6 +197,8 @@ function cadastraTransacao() {
     if (campoSelecionavel.value == "") {
       campoSelecionavel.style.border = "2px solid red";
       popupDeCadastroDeValor.style.display = "flex";
+      paragraphs[1].style.color = 'red'
+      return erroCadastro()
     } else {
       titulo.style.border = "transparent";
     }
@@ -201,6 +206,9 @@ function cadastraTransacao() {
     if (valor.value == "") {
       valor.style.border = "2px solid red";
       popupDeCadastroDeValor.style.display = "flex";
+      paragraphs[2].style.color = 'red'
+
+      return erroCadastro()
     } else {
       titulo.style.border = "transparent";
     }
@@ -208,6 +216,8 @@ function cadastraTransacao() {
     if (categoria.value == "") {
       categoria.style.border = "2px solid red";
       popupDeCadastroDeValor.style.display = "flex";
+      paragraphs[3].style.color = 'red'
+      return erroCadastro()
     } else {
       titulo.style.border = "transparent";
     }
@@ -236,7 +246,7 @@ function cadastraTransacao() {
     let row = {
       descricao: titulo.value,
       valor: valor.value,
-      categoria: valor.value,
+      categoria: categoria.value,
       date: pegarDataDoCadastro(),
     };
 
